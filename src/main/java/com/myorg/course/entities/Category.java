890d838eb4ad4772407fd77ad390cf8b439f9026
Jhonas Alves <jhonas.alves@myorgtech.com>
2022-07-06 14:19:00 -0300

    Product

--- src/main/java/com/myorg/course/entities/Category.java
+++ src/main/java/com/myorg/course/entities/Category.java
@@ -1,7 +1,9 @@
 package com.myorg.course.entities;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -11,6 +13,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -34,6 +38,10 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/myorg/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.myorg.course.services;
+
+import com.myorg.course.entities.Product;
+import com.myorg.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
